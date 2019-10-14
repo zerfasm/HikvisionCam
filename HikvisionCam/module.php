f317667a4d5d35f67f2db69e2efa4ab7d4aeb1f9
zerfasm <56298551+zerfasm@users.noreply.github.com>
2019-10-14 20:05:12 +0200

    Add files via upload

---
+++ HikvisionCam/module.php
@@ -0,0 +1,22 @@
+<?php
+	class HikvisionCam extends IPSModule {
+
+		public function Create()
+		{
+			//Never delete this line!
+			parent::Create();
+		}
+
+		public function Destroy()
+		{
+			//Never delete this line!
+			parent::Destroy();
+		}
+
+		public function ApplyChanges()
+		{
+			//Never delete this line!
+			parent::ApplyChanges();
+		}
+
+	}
---
+++ README.md
@@ -0,0 +1,6 @@
+# HikvisionCam
+
+Folgende Module beinhaltet das HikvisionCam Repository:
+
+- __HikvisionCam__ ([Dokumentation](HikvisionCam))
+	Kurze Beschreibung des Moduls.
