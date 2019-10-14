ba33dcccf37866fd3af89c03af518f8e5871bcd2
zerfasm <56298551+zerfasm@users.noreply.github.com>
2019-10-14 20:15:24 +0200

    Update module.php

--- HikvisionCam/module.php
+++ HikvisionCam/module.php
@@ -5,6 +5,19 @@
 		{
 			//Never delete this line!
 			parent::Create();
+			// Camera Parameter
+        		$this->RegisterPropertyString('IP', 0);
+        		$this->RegisterPropertyString('Name', 0);
+        		$this->RegisterPropertyString('User', 0);
+        		$this->RegisterPropertyString('Password', 0);
+        		$this->RegisterPropertyString('ISAPI', 0);
+        		$this->RegisterPropertyString('Preset', 0);
+			
+			// Snapshot Parameter
+        		$this->RegisterPropertyInteger('No_Picture', 0);
+        		$this->RegisterPropertyString('URL', 0);
+        		$this->RegisterPropertyInteger('Break', 0);
+        		$this->RegisterPropertyString('Picture_Path', 0);
 		}
 
 		public function Destroy()
