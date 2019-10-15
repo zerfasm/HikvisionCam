cd8f5d048a6ac1b2d188eb9e7e8ba52182085420
zerfasm <56298551+zerfasm@users.noreply.github.com>
2019-10-15 18:20:12 +0200

    Update module.php

--- HikvisionCam/module.php
+++ HikvisionCam/module.php
@@ -7,17 +7,17 @@
 			parent::Create();
 			
 			// Camera Parameter
-        		$this->RegisterPropertyString('IP', "192.168.2.62");
+        		$this->RegisterPropertyString('IPAdress', "192.168.2.62");
         		$this->RegisterPropertyString('Name', "Hikvision Cam");
-        		$this->RegisterPropertyString('User', "admin");
-        		$this->RegisterPropertyString('Password', "");
+        		$this->RegisterPropertyString('UserName', "admin");
+        		$this->RegisterPropertyString('UserPassword', "");
         		$this->RegisterPropertyString('ISAPI', "/ISAPI/PTZCtrl/channels/1/presets/2/goto");
         		$this->RegisterPropertyString('Preset', "");
 			
 			// Snapshot Parameter
-        		$this->RegisterPropertyInteger('No_Picture', "9");
+        		$this->RegisterPropertyInteger('No_Picture', 9);
         		$this->RegisterPropertyString('URL', "http://192.168.2.62/Streaming/channels/1/picture");
-        		$this->RegisterPropertyInteger('Break', "100");
+        		$this->RegisterPropertyInteger('Break', 100);
         		$this->RegisterPropertyString('Picture_Path', "D:\Cams\");
 		}
 
