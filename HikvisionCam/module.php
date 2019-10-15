34253f7f9d7ad071ac966fa179085d1a9f9d7c27
zerfasm <56298551+zerfasm@users.noreply.github.com>
2019-10-15 18:18:46 +0200

    Update module.php

--- HikvisionCam/module.php
+++ HikvisionCam/module.php
@@ -5,6 +5,7 @@
 		{
 			//Never delete this line!
 			parent::Create();
+			
 			// Camera Parameter
         		$this->RegisterPropertyString('IP', "192.168.2.62");
         		$this->RegisterPropertyString('Name', "Hikvision Cam");
@@ -14,9 +15,9 @@
         		$this->RegisterPropertyString('Preset', "");
 			
 			// Snapshot Parameter
-        		$this->RegisterPropertyInteger('No_Picture', 9);
+        		$this->RegisterPropertyInteger('No_Picture', "9");
         		$this->RegisterPropertyString('URL', "http://192.168.2.62/Streaming/channels/1/picture");
-        		$this->RegisterPropertyInteger('Break', 100);
+        		$this->RegisterPropertyInteger('Break', "100");
         		$this->RegisterPropertyString('Picture_Path', "D:\Cams\");
 		}
 
