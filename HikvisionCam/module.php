d37b146678e3f45e697d51ea50d75d528c5f27b4
zerfasm <56298551+zerfasm@users.noreply.github.com>
2019-10-19 21:32:04 +0200

    Update module.php

--- HikvisionCam/module.php
+++ HikvisionCam/module.php
@@ -42,11 +42,8 @@ class HikvisionCam extends IPSModule
 	public function ApplyChanges()
 	{
 		//Never delete this line!
-		parent::ApplyChanges();    
-	}
-	
-	public function Update()
-    	{
+		parent::ApplyChanges();  
+		
 		//User
 		$user = $this->ReadPropertyString('UserName');
 
@@ -86,8 +83,10 @@ class HikvisionCam extends IPSModule
 		//Messagetexte und Titel
 		$text 	= $this->ReadPropertyString('Messenger_Text').date("d.m.y - H:i:s");
 		$titel	= $this->ReadPropertyString('Messenger_Title');
-		
-		
+	}
+	
+	public function Update()
+    	{
 		//Go to preset
 		$xml_data = '<PTZPreset version="2.0" xmlns="http://www.isapi.org/ver20/XMLSchema">              
 		</PTZPreset>'."\r\n";
