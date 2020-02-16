2d74e6cdfbd0e7c3b9f268b59c1207980bc31e33
zerfasm <56298551+zerfasm@users.noreply.github.com>
2020-02-16 13:30:28 +0100

    Update module.php

--- HikvisionCam/module.php
+++ HikvisionCam/module.php
@@ -39,10 +39,10 @@ class HikvisionCam extends IPSModule
 		//$this->RegisterTimer('UpdateTrigger', 0, "HKVC_Update(\$_IPS['TARGET']);");
 		
 		//Startposition Cam
-		$this->RegisterPropertyInteger('StartPos', '');
+		$this->RegisterPropertyInteger('StartPos', 2);
 		
 		//Zielposition Cam
-		$this->RegisterPropertyInteger('ZielPos', '');		
+		$this->RegisterPropertyInteger('ZielPos', 1);		
 		
 	}
 
